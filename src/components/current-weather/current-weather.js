02b15551764a7e575fc98d8084ab4e696f8e8c95
Luciano Vazquez <vazquezluciano5@gmail.com>
2022-10-14 14:15:19 -0300

    completed the hard coded part of the top information

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -1,15 +1,21 @@
-import "./current-weather.css"
+import "./current-weather.css";
 
 const CurrentWeather = () => {
-    return (
-        <div className="weather">
-            <div className="top">
-                <p className="city">Boston</p>
-                <p className="weather-description">Sunny</p>
-            </div>
-            <img alt="weather logo or smth" className="weather-icon" src="../icons/01d.png"/>
+  return (
+    <div className="weather">
+      <div className="top">
+        <div>
+          <p className="city">Boston</p>
+          <p className="weather-description">Sunny</p>
         </div>
-    );
-}
+        <img
+          alt="weather logo or smth"
+          className="weather-icon"
+          src="../icons/01d.png"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default CurrentWeather;
